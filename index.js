dfc8b86bd1ee57a8c616ea37988c5f692398605b
JukutPremium <diandradiandra218@gmail.com>
2024-12-19 16:47:45 +0700

    UPDATE index.js

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const client = new Client({
   ]
 });
 
-// Daftar API keys
 const YOUTUBE_API_KEYS = [
   process.env.YOUTUBE_API_KEY_1,
   process.env.YOUTUBE_API_KEY_2,
@@ -34,10 +33,8 @@ const YOUTUBE_API_KEYS = [
   process.env.YOUTUBE_API_KEY_17,
   process.env.YOUTUBE_API_KEY_18,
   process.env.YOUTUBE_API_KEY_19,
-  // Tambahkan key lainnya jika diperlukan
 ];
 
-// Variabel untuk melacak status API key
 const apiKeyStatus = YOUTUBE_API_KEYS.map((key, index) => ({
   key: key,
   usable: true,
@@ -45,7 +42,6 @@ const apiKeyStatus = YOUTUBE_API_KEYS.map((key, index) => ({
   index: index
 }));
 
-// Load monitored channels
 let monitoredChannels = [];
 try {
   const data = fs.readFileSync('data.json', 'utf8');
@@ -54,7 +50,6 @@ try {
   console.error('Error loading monitored channels:', error);
 }
 
-// Load announced videos
 let announcedVideos = [];
 try {
   announcedVideos = JSON.parse(fs.readFileSync('announcedVideos.json', 'utf8') || '[]');
@@ -66,7 +61,6 @@ function saveAnnouncedVideos() {
   fs.writeFileSync('announcedVideos.json', JSON.stringify(announcedVideos, null, 2));
 }
 
-// Fungsi untuk mendapatkan API key yang dapat digunakan
 function getUsableAPIKey() {
   const currentTime = Date.now();
 
@@ -89,7 +83,6 @@ function getUsableAPIKey() {
   return YOUTUBE_API_KEYS[0];
 }
 
-// Fungsi untuk menandai API key sebagai tidak dapat digunakan
 function markAPIKeyAsInvalid(apiKey) {
   const keyStatus = apiKeyStatus.find(status => status.key === apiKey);
 
@@ -178,15 +171,12 @@ async function checkLatestVideos() {
             );
         }
 
-        // Kirimkan pesan ke saluran Discord
         const sentMessage = await announcementChannel.send({ content: message, embeds: [embed] });
         
-        // Menambahkan log informasi tentang konten yang dikirim
         console.log(`[${new Date().toISOString()}] Sent message to Discord channel: ${discordChannelId}`);
         console.log(`[${new Date().toISOString()}] Message content: ${message}`);
         console.log(`[${new Date().toISOString()}] Embed title: ${embed.data.title}`);
 
-        // Tambahkan ID video ke announcedVideos setelah pengumuman
         announcedVideos.push(videoId);
         saveAnnouncedVideos();
 
@@ -216,7 +206,12 @@ client.on('ready', () => {
     type: ActivityType.Listening,
   });
 
-  setInterval(checkLatestVideos, 600000); // Cek setiap 10 menit
+  setInterval(checkLatestVideos, 600000);
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+/*
+ * Copyright (c) 2024 jukutpremium
+ * This project is licensed under the MIT License - see the LICENSE file for details
+ */
